77c5956b5ffb3bfe065c665f6a87816708ff8daf
Milancodeert <milan.stassen2012@gmail.com>
2024-09-28 10:46:09 +0200

    Update python.py

--- python.py
+++ python.py
@@ -4,3 +4,6 @@ def naam_en_leeftijd():
     print("Jouw naam is", naam, "en jouw leeftijd is", leeftijd)
     
 naam_en_leeftijd()
+def vraag_input(onderwerp):
+    input("geef me een onderwerp:")
+    return input("geef me een" + onderwerp + ": ")
